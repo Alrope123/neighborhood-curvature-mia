9ea010fd1d036a3b105a4ae253a8f71a934b1474
Shane Lyu <alrope@cs.washington.edu>
2023-08-23 16:52:31 -0700

    updated pile counting script

--- debug/count_pile.py
+++ debug/count_pile.py
@@ -7,10 +7,13 @@ import os
 if __name__ == "__main__":
     import zstandard as zstd
     DCTX = zstd.ZstdDecompressor(max_window_size=2**31)
-    count = 0
+    document_count = 0
+    paragraph_count = 0
     with zstd.open("/gscratch/h2lab/sewon/data/the-pile/val.jsonl.zst", mode='rb', dctx=DCTX) as zfh, io.TextIOWrapper(zfh) as iofh:
         for i, line in enumerate(iofh):
-            count += 1
-    print(count)
+            document_count += 1
+            paragraph_count += len(line.split('\n'))
+    print(document_count)
+    print(paragraph_count)
 
 # python generate_predctions.py --model_name togethercomputer/RedPajama-INCITE-Base-3B-v1 --n 1000 --answer_n 3
