a05301a1886e1f48bed89b9d7c79846a4ef5725d
Shane Lyu <alrope@uw.edu>
2023-08-26 15:57:57 -0700

    updated checking code

--- debug/count_pile.py
+++ debug/count_pile.py
@@ -7,17 +7,28 @@ import zstandard as zstd
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default="/gscratch/h2lab/sewon/data/the-pile/val.jsonl.zst")
-    
+    parser.add_argument('--check_id', action="store_true", default=False)
+
     args = parser.parse_args()
 
     DCTX = zstd.ZstdDecompressor(max_window_size=2**31)
     document_count = 0
     paragraph_count = 0
     samples = []
+    unmatched = []
+    j = 0
     with zstd.open(args.data_path, mode='rb', dctx=DCTX) as zfh, io.TextIOWrapper(zfh) as iofh:
         for i, line in enumerate(iofh):
             document_count += 1
-            paragraph_count += len(line.split('\n'))
+            dp = json.loads(line)
+            paragraph_count += len(dp['text'])
+            if args.check_id:
+                assert j <= dp['id']
+                while j != dp['id']:
+                    unmatched.append(j)
+                    j += 1
+                j += 1
+                assert j == dp['id'] + 1
             if i < 10:
                 samples.append(line)
     print(document_count)
