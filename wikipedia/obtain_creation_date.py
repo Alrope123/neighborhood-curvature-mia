417ed3e8465519ed0344d375ea2dd5fcd562821b
Shane Lyu <alrope@cs.washington.edu>
2023-09-01 14:16:44 -0700

    edited debugging code

--- wikipedia/obtain_creation_date.py
+++ wikipedia/obtain_creation_date.py
@@ -5,8 +5,6 @@ from urllib.parse import quote
 def get_wikipedia_creation_timestamp(article_title):
     # URL encoding the article title to properly format the URL
     encoded_title = quote(article_title)
-    
-    print(encoded_title)
 
     # Create URL for the Wikipedia information page of the article
     info_url = f"https://en.wikipedia.org/w/index.php?title={encoded_title}&action=info"
@@ -36,6 +34,13 @@ def get_wikipedia_creation_timestamp(article_title):
 
 if __name__ == '__main__':
     article_title = "Barack Obama"
-    timestamp = get_wikipedia_creation_timestamp(article_title)
-    if timestamp:
-        print(f"The article '{article_title}' was created on: {timestamp}")
+    import time
+    n_iterations = 100
+    startTime = time.time()
+    for i in range(n_iterations):
+        timestamp = get_wikipedia_creation_timestamp(article_title)
+    # if timestamp:
+    #     print(f"The article '{article_title}' was created on: {timestamp}")
+    endTime = time.time()
+    time_taken = endTime - startTime
+    print("Used {} to process {} articles. Average rate: {} articles/s".format(time_taken), n_iterations, n_iterations/time_taken)
