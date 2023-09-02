9dd08e3849cec753a7b3a63eb2a330b9aa603ab1
Shane Lyu <alrope@cs.washington.edu>
2023-09-01 18:48:44 -0700

    added commands for obtaining date

---
+++ wikipedia/commands.sh
@@ -0,0 +1,2 @@
+python wikipedia/obtain_creation_date.py --set_name pile_nonmember_text.pkl --downsample_factor 0.1
+python wikipedia/obtain_creation_date.py --set_name pile_member_text.pkl --downsample_factor 0.1
