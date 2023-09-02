f7f3c88fa95d468a9bb646606e084480e99f4f52
Shane Lyu <alrope@cs.washington.edu>
2023-09-01 17:34:10 -0700

    made label membering more robust

--- wikipedia/label_members.py
+++ wikipedia/label_members.py
@@ -4,6 +4,18 @@ import pickle as pkl
 import json
 from tqdm import tqdm
 
+def camel_case_split(str):
+    words = [[str[0]]]
+ 
+    for c in str[1:]:
+        if words[-1][-1].islower() and c.isupper():
+            words.append(list(c))
+        else:
+            words[-1].append(c)
+ 
+    return ' '.join([''.join(word) for word in words])
+     
+
 def iterate_files(root_dir):
     file_paths = []
     for root, _, files in os.walk(root_dir):
@@ -25,35 +37,54 @@ if __name__ == '__main__':
         pile_set = pkl.load(f)
 
     # initialize the member/non-member sets
-    member_path = os.path.join(args.out_dir, 'pile_member.pkl')
-    non_member_path = os.path.join(args.out_dir, 'pile_nonmember.pkl')
-    if os.path.exists(member_path) and os.path.exists(non_member_path):
-        with open(member_path, 'rb') as f:
-            member_set = pkl.load(f)
-        with open(non_member_path, 'rb') as f:
-            non_member_set = pkl.load(f)  
+    member_text_path = os.path.join(args.out_dir, 'pile_member_text.pkl')
+    non_member_text_path = os.path.join(args.out_dir, 'pile_nonmember_text.pkl')
+    member_notext_path = os.path.join(args.out_dir, 'pile_member_text.pkl')
+    non_member_notext_path = os.path.join(args.out_dir, 'pile_nonmember_text.pkl')
+    if os.path.exists(member_text_path) and os.path.exists(non_member_text_path) and \
+        os.path.exists(member_notext_path) and os.path.exists(non_member_notext_path):
+        with open(member_text_path, 'rb') as f:
+            member_text_set = pkl.load(f)
+        with open(non_member_text_path, 'rb') as f:
+            non_member_text_set = pkl.load(f)
+        with open(member_notext_path, 'rb') as f:
+            member_notext_set = pkl.load(f)
+        with open(non_member_notext_path, 'rb') as f:
+            non_member_notext_set = pkl.load(f)  
     else:
-        member_set = set()
-        non_member_set = set()
+        member_text_set = set()
+        non_member_text_set = set()
+        member_notext_set = set()
+        non_member_notext_set = set()
         # Go through each file and label them based on title
         for file_path in tqdm(iterate_files(args.data_dir)):
             with open(file_path, 'r') as f:
                 for line in f:
                     dp = json.loads(line)
-                    # if dp['text'] != "":
-                    if True:
-                        title = dp['title']
-                        if title in pile_set:
-                            member_set.add(title)
+                    title = dp['title']
+                    if title in pile_set or camel_case_split(title) in pile_set:
+                        if dp['text'] != "":
+                            member_text_set.add(title)
                         else:
-                            non_member_set.add(title)
-        with open(member_path, 'wb') as f:
-            pkl.dump(member_set, f)
-
-        with open(non_member_path, 'wb') as f:
-            pkl.dump(non_member_set, f)
+                            member_notext_set.add(title)
+                    else:
+                        if dp['text'] != "":
+                            non_member_text_set.add(title)
+                        else:
+                            non_member_notext_set.add(title)
+        # Save
+        with open(member_text_path, 'wb') as f:
+            pkl.dump(member_text_set, f)
+        with open(non_member_text_path, 'wb') as f:
+            pkl.dump(non_member_text_set, f)
+        with open(member_notext_path, 'wb') as f:
+            pkl.dump(member_notext_set, f)
+        with open(non_member_notext_path, 'wb') as f:
+            pkl.dump(non_member_notext_set, f)
 
     print("Total # articles in the pile: {}".format(len(pile_set)))
-    print("# articles from newest Wikipedia dump in the pile: {}".format(len(member_set)))
-    print("# articles from newest Wikipedia dump NOT in the pile: {}".format(len(non_member_set)))
+    print("# articles from newest Wikipedia dump in the pile with text available: {}".format(len(member_text_set)))
+    print("# articles from newest Wikipedia dump NOT in the pile: {} with text available:".format(len(non_member_text_set)))
+    print("# articles from newest Wikipedia dump in the pile without text available: {}".format(len(member_notext_set)))
+    print("# articles from newest Wikipedia dump NOT in the pile: {} without text available:".format(len(non_member_notext_set)))
 
