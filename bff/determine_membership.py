753b95f91a4a256f60fea511fe6c87bef36f93af
Shane Lyu <alrope@cs.washington.edu>
2023-09-07 01:22:58 -0700

    edited script

--- bff/determine_membership.py
+++ bff/determine_membership.py
@@ -34,7 +34,7 @@ def get_group(dp, data_type):
         raise NotImplementedError('The data type is not implemented yet.')
 
 
-def draw_histogram(data, save_path, bins=None, title=None, xlabel=None, ylabel=None, cumulative=False, x_interval=0.05):
+def draw_histogram(data, save_path, bins=None, title=None, xlabel=None, ylabel=None, cumulative=False, x_interval=-1):
     """Draw a histogram for the given data."""
     
     plt.figure(figsize=(10,6))  # Set the figure size
@@ -44,8 +44,9 @@ def draw_histogram(data, save_path, bins=None, title=None, xlabel=None, ylabel=N
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
-    x_ticks = np.arange(min(data), max(data) + x_interval, x_interval)
-    plt.xticks(x_ticks)
+    if x_interval > 0:
+        x_ticks = np.arange(min(data), max(data) + x_interval, x_interval)
+        plt.xticks(x_ticks)
     
     # plt.grid(axis='y', alpha=0.75)  # Add a grid on y-axis
     plt.savefig(save_path, format='png') 
@@ -68,64 +69,71 @@ def main(args):
         os.mkdir(save_dir)
     filter_names = os.listdir(overlap_dir)    
 
-    # Process each file
-    print("Going through each file to check BFF results...")
-    group_to_member = {}
-    for i, filename in enumerate(tqdm(os.listdir(data_dir))):
-        # DEBUG:
-        # if i > 10:
-        #     break
 
-        # Figure out the path
-        data_path = os.path.join(data_dir, filename)
-        
-
-        # Load in the data
-        data = custom_open(data_path)
-
-        is_member_all = [False] * len(data)
-        for filter_name in filter_names:
-            overlap_path = os.path.join(overlap_dir, filter_name, filename)
-            assert os.path.exists(overlap_path)
-
-            # Read in each overlap file
-            overlap_data = custom_open(overlap_path)
-            assert len(data) == len(overlap_data)
-            coverages = [calculate_coverage(dp) for dp in overlap_data]
-            # Draw the distribution of overlaps if haven't drawn
-            if not drawn:
-                draw_histogram(coverages, title=None, xlabel="Percentage of duplication", ylabel="# Documents(k)", save_path=os.path.join(save_dir, 'overlap_distribution.png'), bins=50)
-                draw_histogram(coverages, title=None, xlabel="Percentage of duplication", save_path=os.path.join(save_dir, 'overlap_distribution_CDF.png'), bins=50, cumulative=True)
-                drawn = True
-            is_member = [coverage > threshold for coverage in coverages]
+    membership_info_path = os.path.join(save_dir, 'group_to_member.pkl')
+    if not os.path.exists(membership_info_path):
+        # Process each file
+        print("Going through each file to check BFF results...")
+        group_to_member = {}
+        for i, filename in enumerate(tqdm(os.listdir(data_dir))):
             # DEBUG:
-            # print("Filter {}:".format(filter_name))
-            # print("\taverage coverage: {}".format(np.mean(coverages)))
-            # print("\tmembers: {} / {}".format(sum(is_member), len(is_member)))
-            # print("\tall members: {} / {}".format(sum(is_member_all), len(is_member_all)))
-            assert len(is_member_all) ==  len(is_member)
-            is_member_all = [a or b for a, b in zip(is_member_all, is_member)]
+            if i > 10:
+                break
 
-        with open(os.path.join(save_dir, '{}.pkl'.format(filename)), "wb") as f:
-            pkl.dump(is_member_all, f)
+            # Figure out the path
+            data_path = os.path.join(data_dir, filename)
+            
 
-        for i, dp in enumerate(data):
-            group = get_group(dp, data_type)
-            if group not in group_to_member:
-                group_to_member[group] = []
-            group_to_member[group].append((filename, i, is_member_all[i]))
+            # Load in the data
+            data = custom_open(data_path)
 
-        # DEBUG:
-        # print("Total members: {} / {}".format(sum(is_member_all), len(is_member_all)))
+            is_member_all = [False] * len(data)
+            for filter_name in filter_names:
+                overlap_path = os.path.join(overlap_dir, filter_name, filename)
+                assert os.path.exists(overlap_path)
+
+                # Read in each overlap file
+                overlap_data = custom_open(overlap_path)
+                assert len(data) == len(overlap_data)
+                coverages = [calculate_coverage(dp) for dp in overlap_data]
+                # Draw the distribution of overlaps if haven't drawn
+                if not drawn:
+                    draw_histogram(coverages, title=None, xlabel="Percentage of duplication", ylabel="# Documents(k)",
+                                    save_path=os.path.join(save_dir, 'overlap_distribution.png'), bins=50, x_interval=0.02)
+                    draw_histogram(coverages, title=None, xlabel="Percentage of duplication",
+                                    save_path=os.path.join(save_dir, 'overlap_distribution_CDF.png'), bins=50, cumulative=True, x_interval=0.02)
+                    drawn = True
+                is_member = [coverage > threshold for coverage in coverages]
+                assert len(is_member_all) ==  len(is_member)
+                is_member_all = [a or b for a, b in zip(is_member_all, is_member)]
+
+            with open(os.path.join(save_dir, '{}.pkl'.format(filename)), "wb") as f:
+                pkl.dump(is_member_all, f)
+
+            for i, dp in enumerate(data):
+                group = get_group(dp, data_type)
+                if group not in group_to_member:
+                    group_to_member[group] = []
+                group_to_member[group].append((filename, i, is_member_all[i]))
+
+        # Save the membership info
+        with open(membership_info_path, "wb") as f:
+            pkl.dump(group_to_member, f)
+    
+    else:
+        with open(membership_info_path, "rb") as f:
+            group_to_member = pkl.load(f)
 
     # Create statistic info
     print("Calculating the statistics...")
     group_lengths = [len(members) for _, members in group_to_member.items()]
-    group_member_rate = [sum([member[2] for member in members]) / len(members) for group, members in group_to_member.items()]
+    group_member_rate = [sum([member[2] for member in members]) / len(members) for _, members in group_to_member.items()]
+    sorted_group = sorted(group_to_member.items(), key=lambda x: x[0])
     stats = {
         "threshold": threshold,
         "number of groups": len(group_to_member),
-        "examples of group": list(group_to_member.keys())[:15],
+        "first n group": sorted_group[:15],
+        "last n group": sorted_group[-15:],
         "number of group with all members": sum([rate == 1.0 for rate in group_member_rate]),
         "number of group with all non-members": sum([rate == 0.0 for rate in group_member_rate]),
         "average number of instance in every group": np.mean(group_lengths),
@@ -134,11 +142,20 @@ def main(args):
     print(stats)
     with open(os.path.join(save_dir, 'stats.json'), "w") as f:
         json.dump(stats, f)
-    draw_histogram(group_member_rate, title=None, xlabel="Percentage of Members", ylabel="# Dates(k)", save_path=os.path.join(save_dir, 'memership_distribution.png'), bins=20)
-
-    # Save the membership info
-    with open(os.path.join(save_dir, 'group_to_member.pkl'), "wb") as f:
-        pkl.dump(group_to_member, f)
+    draw_histogram(group_lengths, title=None, xlabel="# documents each date", ylabel="# Dates(k)",
+                    save_path=os.path.join(save_dir, 'documents_date_distribution.png'))
+    draw_histogram(group_member_rate, title=None, xlabel="Percentage of Members", ylabel="# Dates(k)",
+                    save_path=os.path.join(save_dir, 'memership_distribution.png'), bins=20, x_interval=0.02)
+    
+    # Create the check map
+    check_map_path = os.path.join(save_dir, 'check_map.pkl')
+    if not os.path.exists(check_map_path):
+        check_map = {}
+        for group, members in group_to_member.items():
+            for (filename, i, is_member) in members:
+                check_map[filename, i] = (group, is_member)
+        with open(check_map_path, "wb") as f:
+            pkl.dump(check_map, f)
 
     
 if __name__ == '__main__':
