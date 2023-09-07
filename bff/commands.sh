80db0d3deacc535d56ab3b0d0db158cef78e650d
Shane Lyu <alrope@cs.washington.edu>
2023-09-07 01:54:01 -0700

    edited command

--- bff/commands.sh
+++ bff/commands.sh
@@ -1,3 +1,3 @@
-for threshold in 0.5 0.3 0.1 0.03 0.01 0.005 0.003 0.001 ; do
+for threshold in 0.5 0.3 0.1 0.05 0.03 0.01 0.005 0.003 0.001 ; do
     python bff/determine_membership.py --threshold $threshold
 done
