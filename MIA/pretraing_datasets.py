05a78961acdeab57d42d749bd2433f96870a440b
Shane Lyu <alrope@cs.washington.edu>
2023-09-14 01:14:28 -0700

    fixed bug

--- MIA/pretraing_datasets.py
+++ MIA/pretraing_datasets.py
@@ -36,7 +36,7 @@ def sample_group(membership_info, n_group=100, n_document_per_group=30, train=Tr
         if group in groups:
             new_added_data = []
             for filename, i, _, _ in infos['documents']:
-                new_added_data.add((filename, i))
+                new_added_data.append((filename, i))
             # print(new_added_data)
             random.shuffle(new_added_data)
             new_added_data = new_added_data[:n_document_per_group]
