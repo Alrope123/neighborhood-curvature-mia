a7236b8a75bee4fa9dbbea92d224f33688ce9e80
Shane Lyu <alrope@cs.washington.edu>
2023-09-16 17:44:11 -0700

    added arxiv processing script

---
+++ arxiv/fix_arxiv_format.py
@@ -0,0 +1,18 @@
+import json
+
+def fix_newline_format(text):
+    return text.replace('\n', '')
+
+if __name__ == '__main__':
+    with open("/gscratch/h2lab/alrope/neighborhood-curvature-mia/debug/out/same_arxiv_document.json", 'r') as f:
+        data = json.load(f)
+    keys = list(data.keys())
+    selected_key = None
+    for key in keys:
+        if "Strongly multiplicative linear secret sharing" in key:
+            selected_key = key
+    text = data[selected_key][0]
+    filtered_text = fix_newline_format(text)
+
+    with open("/gscratch/h2lab/alrope/neighborhood-curvature-mia/debug/out/filtered_text.json", 'r') as f:
+        json.dump({"filtered_text": filtered_text}, f)
