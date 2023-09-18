c28efb659b4c8b26c5d12ada20c39ff0004100c3
Shane Lyu <alrope@cs.washington.edu>
2023-09-18 15:09:53 -0700

    fixed bug

--- MIA/process_results.py
+++ MIA/process_results.py
@@ -50,7 +50,7 @@ def save_ll_histograms(members, nonmembers, name, n_bins, SAVE_FOLDER):
     # assert len(members) == len(nonmembers)
     # first, clear plt
     plt.clf()
-    bins = int((max(nonmembers) - min(members)) / n_bins) 
+    bins = int(abs(max(nonmembers) - min(members)) / n_bins) 
 
     # plot histogram of sampled/perturbed sampled on left, original/perturbed original on right
     plt.figure(figsize=(10, 6))
@@ -154,11 +154,11 @@ if __name__ == '__main__':
                 elif loss == 'bff':
                     for group, predictions in group_results_members.items():
                         if len(predictions) >= top_k:
-                            scores = [1.0-score for (_, _, score, _) in group_to_documents[group]['documents']]
+                            scores = [score for (_, _, score, _) in group_to_documents[group]['documents']]
                             cur_member_predictions.append(calculate_group_loss(scores, method, top_k))
                     for group, predictions in group_results_nonmembers.items():
                         if len(predictions) >= top_k:
-                            scores = [1.0-score for (_, _, score, _) in group_to_documents[group]['documents']]
+                            scores = [score for (_, _, score, _) in group_to_documents[group]['documents']]
                             cur_nonmember_predictions.append(calculate_group_loss(scores, method, top_k))
                 random.shuffle(cur_member_predictions)
                 random.shuffle(cur_nonmember_predictions)
