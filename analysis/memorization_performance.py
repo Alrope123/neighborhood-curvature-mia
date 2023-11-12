bd02c2a45039fc20ebd5c3ac816016707e392f01
Shane Lyu <alrope@uw.edu>
2023-11-11 16:46:29 -0800

    edited analysis script

--- analysis/memorization_performance.py
+++ analysis/memorization_performance.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import pickle as pkl
 import json
-from sklearn.metrics import roc_curve, precision_recall_curve, auc
+from sklearn.metrics import roc_curve, precision_recall_curve, auc, precision_score, recall_score
 import math
 
 
@@ -10,9 +10,9 @@ def get_roc_metrics(real_preds, sample_preds):
     real_preds =  [element for element in real_preds if not math.isnan(element)]
     sample_preds = [element for element in sample_preds if not math.isnan(element)]
 
-    fpr, tpr, _ = roc_curve([0] * len(real_preds) + [1] * len(sample_preds), real_preds + sample_preds)
+    fpr, tpr, thresholds = roc_curve([0] * len(real_preds) + [1] * len(sample_preds), real_preds + sample_preds)
     roc_auc = auc(fpr, tpr)
-    return fpr.tolist(), tpr.tolist(), float(roc_auc)
+    return fpr.tolist(), tpr.tolist(), thresholds.tolist(), float(roc_auc)
 
 
 if __name__ == '__main__':
@@ -31,13 +31,32 @@ if __name__ == '__main__':
         for i, entry in enumerate(result["member_meta"]):
             lls[size][tuple(entry)] = result["member_lls"][i]
         
-    # Set a threshold
+    # Find a threshold
     best_nonmember_lls = lls[sizes[-1]].values()
     best_member_lls = lls[sizes[-1]].values()
+    threshold = None
+    fpr, tpr, thresholds, roc_auc = get_roc_metrics(best_nonmember_lls,  best_member_lls)
+    for i, (rate, thresholds) in zip(fpr, roc_auc):
+        if rate > 0.05:
+            threshold = thresholds[i-1]
     
-    fpr, tpr, roc_auc = get_roc_metrics(best_nonmember_lls,  best_member_lls)
-    print(fpr[:10])
-    print(fpr[-10:])
+    # Determine memorized label v.s. not memorized label
+    labels = {}
+    for entry, score in lls[sizes[-1]].items():
+        labels[tuple(entry)] = score > threshold
+    
+    # Collecting Evaluation
+    evaluations = []
+    for size, size_result in lls.items():
+        predictions = {}
+        for entry, score in size_result.items():
+            predictions[tuple(entry)] = score > threshold
+        precision = precision_score(list(labels.values()), list(predictions.values()))
+        recall = recall_score(list(labels.values()), list(predictions.values()))
+        f1 = 2 * (precision * recall) / (precision + recall)
+        print("Precision for size ({}): {}".format(size, precision))
+        print("Recall for size ({}): {}".format(size, recall))
+        print("F1 for size ({}): {}".format(size, recall))
 
 
         
