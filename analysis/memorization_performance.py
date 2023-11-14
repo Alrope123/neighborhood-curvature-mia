dca3aaf0bb6507c2125aa0b4d0e8c8e99a755e78
Shane Lyu <alrope@uw.edu>
2023-11-13 22:37:45 -0800

    edited analysis script

--- analysis/memorization_performance.py
+++ analysis/memorization_performance.py
@@ -15,11 +15,13 @@ def get_roc_metrics(real_preds, sample_preds):
     roc_auc = auc(fpr, tpr)
     return fpr.tolist(), tpr.tolist(), thresholds.tolist(), float(roc_auc)
 
+def get_correlation(evaluations, key1, key2="performance"):
+    return np.corrcoef([evaluation[key1] for evaluation in evaluations], [evaluation[key2] for evaluation in evaluations])[0, 1]
 
 if __name__ == '__main__':
     base_result_path = "/gscratch/h2lab/alrope/neighborhood-curvature-mia/results_new/wikipedia_noisy-770-230-100-m1024/EleutherAI_pythia-{}/lr_ratio_threshold_results.json"
     crit_result_path = "/gscratch/h2lab/alrope/neighborhood-curvature-mia/results_new/wikipedia_noisy-770-230-100-m1024/lira-{}-{}/crit/group_out.json"
-    sizes = ["160m", "410m", "1b", "2.8b", "6.9b", "12b--min_k"]
+    sizes = ["14m", "70m", "160m", "410m", "1b", "2.8b", "6.9b", "12b--min_k"]
 
     # Load perplexities
     lls = {size: {} for size in sizes}
@@ -32,42 +34,58 @@ if __name__ == '__main__':
         for i, entry in enumerate(result["member_meta"]):
             lls[size][tuple(entry)] = result["member_lls"][i]
     
-    # Find a threshold
-    best_nonmember_lls = lls[sizes[-1]].values()
-    best_member_lls = lls[sizes[-1]].values()
-    threshold = None
-    fpr, tpr, thresholds, roc_auc = get_roc_metrics(best_nonmember_lls,  best_member_lls)
-    print(fpr[:10])
-    print(fpr[-10:])
-    print(thresholds[:10])
-    print(thresholds[-10:])
-    for i, rate in enumerate(fpr):
-        if rate > 0.05:
-            threshold = thresholds[i-1]
-            break
-
-    # Determine memorized label v.s. not memorized label
-    labels = {}
-    for entry, score in lls[sizes[-1]].items():
-        labels[tuple(entry)] = score > threshold
-    print("Memorized rate for the largest model:{}".format(np.mean(list(labels.values()))))
-    
-    # Collecting Evaluation
     evaluations = []
-    for size, size_result in lls.items():
-        correct_labels = []
-        predictions = []
-        for entry, score in size_result.items():
-            predictions.append(score > threshold)
-            correct_labels.append(labels[tuple(entry)]) 
+    for target_size in reversed(sizes):
+        # Find a threshold
+        best_nonmember_lls = lls[target_size].values()
+        best_member_lls = lls[target_size].values()
+        threshold = None
+        fpr, tpr, thresholds, roc_auc = get_roc_metrics(best_nonmember_lls,  best_member_lls)
+        for i, rate in enumerate(fpr):
+            if rate > 0.05:
+                threshold = thresholds[i-1]
+                break
 
-        precision = precision_score(correct_labels, predictions)
-        recall = recall_score(correct_labels, predictions)
-        f1 = 2 * (precision * recall) / (precision + recall)
-        print("Precision for size ({}): {}".format(size, precision))
-        print("Recall for size ({}): {}".format(size, recall))
-        print("F1 for size ({}): {}".format(size, f1))
-        print("=========================================================")
+        # Determine memorized label v.s. not memorized label
+        labels = {}
+        for entry, score in lls[target_size].items():
+            labels[tuple(entry)] = score > threshold
+        print("Memorized rate for the largest model:{}".format(np.mean(list(labels.values()))))
+        
+        # Collecting Evaluation
+        for ref_size, size_result in lls.items():
+            correct_labels = []
+            predictions = []
+            for entry, score in size_result.items():
+                predictions.append(score > threshold)
+                correct_labels.append(labels[tuple(entry)]) 
 
+            precision = precision_score(correct_labels, predictions)
+            recall = recall_score(correct_labels, predictions)
+            f1 = 2 * (precision * recall) / (precision + recall)
+            peformance_path = crit_result_path.format(target_size, ref_size)
+            with open(peformance_path, 'r') as f:
+                performance = json.load(f)["average"]["max"]["100"]["MIA"]["best"]["ROC AUC"]
+            
+            evaluations.append({
+                "target_size": target_size,
+                "ref_size": ref_size,
+                "precision": precision,
+                "recall": recall,
+                "f1": f1,
+                "performance": performance
+            })
+        
+    correlations = {
+        "evaluations": evaluations,
+        "precision_correlation": get_correlation(evaluations, "precision"),
+        "recall_correlation": get_correlation(evaluations, "precision"),
+        "f1_correlation": get_correlation(evaluations, "precision")
+    }
 
+    output_dir = "/gscratch/h2lab/alrope/neighborhood-curvature-mia/results_analysis/"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(os.path.join(result_path, "memorizaiton.json"), 'r') as f:
+        json.dump(correlations)
         
