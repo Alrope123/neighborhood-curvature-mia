1aaa70f7440c65ae1a84329f084231d38c26b240
Shane Lyu <alrope@uw.edu>
2023-12-13 16:34:13 -0800

    added sampling dataset code

--- instruction/sample_dataset.py
+++ instruction/sample_dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import pickle as pkl
 import argparse
+import random
 from tqdm import tqdm
 from datetime import datetime
 from datasets import load_dataset, concatenate_datasets
@@ -12,8 +13,10 @@ instruction_v2_set = ['code_alpaca', 'hard_coded', 'science.scierc_ner', 'cot', 
 
 
 if __name__ == '__main__':
-    count = {}
+    np.random.seed(2023)
+    random.seed(2023)
 
+    selected_indices = {}
     def filter_rows(row):
         # Replace 'value_to_delete' with the value which, if found, will lead to row deletion
         return row['dataset'] not in instruction_v1_set
@@ -22,12 +25,28 @@ if __name__ == '__main__':
     dataset_v2 = dataset_v2.filter(filter_rows)
     merged_dataset = concatenate_datasets([dataset_v1, dataset_v2])
     
-    for entry in merged_dataset:
+    for i, entry in enumerate(merged_dataset):
         dataset = entry["dataset"]
-        if dataset not in count:
-            count[dataset] = 0
-        count[dataset] = count[dataset] + 1
+        if dataset not in selected_indices:
+            selected_indices[dataset] = []
+        selected_indices[dataset].append(i)
 
-    print(count) 
+    for key, value in selected_indices.items():
+        if len(value) < 1000:
+            continue
+        indices = selected_indices[key]
+        indices.shuffle()
+        selected_indices[key] = indices[:1000]
+
+    new_dataset = []
+    for i, entry in enumerate(merged_dataset):
+        dataset = entry["dataset"]
+        if i in selected_indices[dataset]:
+            new_dataset.append(entry)
+    
+    with open(os.path.join("/gscratch/h2lab/alrope/data/instruction/0.jsonl"), 'w') as f:
+        for entry in new_dataset:
+            f.write(json.dumps(entry) + "\n")
+
 
         
