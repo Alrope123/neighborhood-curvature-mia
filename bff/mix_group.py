ecf111a7b97419299a6a95cd9d31e73928a4a586
Shane Lyu <alrope@uw.edu>
2024-02-15 08:23:26 -0800

    edited mix group script

--- bff/mix_group.py
+++ bff/mix_group.py
@@ -132,10 +132,17 @@ def decide_member_individual(filename, i, score, document_threshold):
     return score > document_threshold
 
 def main(args):
+    np.random.seed(2024)
+
     # Process args
     save_dir = args.save_dir
     data_type = args.data_type
     document_threshold = args.document_threshold
+    member_mix_ratio = args.member_mix_ratio
+    nonmember_mix_ratio = args.nonmember_mix_ratio
+    member_group_size = args.member_group_size
+    nonmember_group_size = args.nonmember_group_size
+    size = args.size
     assert os.path.exists(save_dir)
     save_dir = os.path.join(save_dir, data_type[data_type.index('-')+1 : ] if '@' in data_type else data_type)
     assert os.path.exists(save_dir), save_dir
@@ -143,6 +150,17 @@ def main(args):
     membership_info_path = os.path.join(save_dir, 'group_to_member.pkl')
     with open(membership_info_path, "rb") as f:
         membership_info = pkl.load(f)
+
+    new_save_dir = os.path.join(args.save_dir, "mixture")
+    if not os.path.exists(new_save_dir):
+        os.mkdir(new_save_dir)
+    new_save_dir = os.path.join(new_save_dir, "{}-{}-{}-{}".format(member_mix_ratio, nonmember_mix_ratio, member_group_size, nonmember_group_size))
+    if not os.path.exists(new_save_dir):
+        os.mkdir(new_save_dir)
+    new_membership_info_path = os.path.join(new_save_dir, 'group_to_member.pkl')
+
+    member_groups = []
+    nonmember_groups = []
 
     scores_and_group = []
     for group, infos in membership_info.items():
@@ -162,7 +180,100 @@ def main(args):
                 is_members.append(is_member)
         scores_and_group.append((np.mean(scores), group))
         membership_info[group]['is_members'] = is_members
-        membership_info[group]['group_is_member'] = decide_member_group(np.mean(scores), group, data_type)
+        is_group_member = decide_member_group(np.mean(scores), group, data_type)
+        if is_group_member:
+            member_groups.append(group)
+        else:
+            nonmember_groups.append(group)  
+        membership_info[group]['group_is_member'] = is_group_member
+
+    # filtering
+    member_pollution_size = int(size * member_mix_ratio)
+    nonmember_pollution_size = int(size * nonmember_mix_ratio)
+    member_base_size = size - nonmember_pollution_size
+    nonmember_base_size = size - member_pollution_size
+    
+    good_base_member_groups = [group  for group in member_groups if len(membership_info[group]['documents']) >= member_base_size]
+    good_pollution_member_groups = [group  for group in member_groups if len(membership_info[group]['documents']) >= member_pollution_size]
+    good_base_nonmember_groups = [group  for group in nonmember_groups if len(membership_info[group]['documents']) >= nonmember_base_size]
+    good_pollution_nonmember_groups = [group  for group in nonmember_groups if len(membership_info[group]['documents']) >= nonmember_pollution_size]
+    
+    print([good_base_member_groups, good_pollution_member_groups, good_base_nonmember_groups, good_pollution_nonmember_groups])
+    assert False
+
+    if member_base_size > member_pollution_size:
+        np.random.shuffle(good_base_member_groups)
+        good_base_member_groups = good_base_member_groups[:member_group_size]
+        good_pollution_member_groups = [group for group in good_pollution_member_groups if group not in good_base_member_groups]
+        np.random.shuffle(good_pollution_member_groups)
+        good_pollution_member_groups = good_pollution_member_groups[:nonmember_group_size]
+    else:
+        np.random.shuffle(good_pollution_member_groups)
+        good_pollution_member_groups = good_pollution_member_groups[:nonmember_group_size]
+        good_pollution_member_groups = [group for group in good_pollution_member_groups if group not in good_pollution_member_groups]
+        np.random.shuffle(good_base_member_groups)
+        good_base_member_groups = good_base_member_groups[:member_group_size]
+    
+    if nonmember_base_size > nonmember_pollution_size:
+        np.random.shuffle(good_base_nonmember_groups)
+        good_base_nonmember_groups = good_base_nonmember_groups[:nonmember_group_size]
+        good_pollution_nonmember_groups = [group for group in good_pollution_nonmember_groups if group not in good_base_nonmember_groups]
+        np.random.shuffle(good_pollution_nonmember_groups)
+        good_pollution_nonmember_groups = good_pollution_nonmember_groups[:member_group_size]
+    else:
+        np.random.shuffle(good_pollution_nonmember_groups)
+        good_pollution_nonmember_groups = good_pollution_nonmember_groups[:member_group_size]
+        good_pollution_nonmember_groups = [group for group in good_pollution_nonmember_groups if group not in good_pollution_nonmember_groups]
+        np.random.shuffle(good_base_nonmember_groups)
+        good_base_nonmember_groups = good_base_nonmember_groups[:nonmember_group_size]
+
+    assert len(good_pollution_member_groups) == nonmember_group_size and len(good_base_nonmember_groups) == nonmember_group_size and \
+            len(good_base_member_groups) == member_group_size and len(good_pollution_nonmember_groups) == member_group_size, \
+            [len(good_pollution_member_groups), len(good_base_nonmember_groups), len(good_base_member_groups), len(good_pollution_nonmember_groups)]
+
+    new_membership_info = {}
+    for i, group in enumerate(good_base_member_groups):
+        other_group = good_pollution_nonmember_groups[i]
+
+        documents = membership_info[group]['documents']
+        assert len(documents) > member_base_size
+        np.random.shuffle(documents)
+        final_documents = documents[:member_base_size]
+        final_membership = [True] * member_base_size
+
+        documents = membership_info[other_group]['documents']
+        assert len(documents) > nonmember_pollution_size
+        np.random.shuffle(documents)
+        final_documents.extends(documents[:nonmember_pollution_size])
+        final_membership.extends([False] * nonmember_pollution_size)
+
+        membership_info[group]['documents'] = final_documents
+        membership_info[group]['is_members'] = final_documents
+        new_membership_info[group] = membership_info[group]
+        assert len(new_membership_info[group]['documents']) == size, (len(new_membership_info[group]['documents']), size)
+        assert len(new_membership_info[group]['is_members']) == size, (len(new_membership_info[group]['is_members']), size)
+
+    for i, group in enumerate(good_base_nonmember_groups):
+        other_group = good_pollution_member_groups[i]
+
+        documents = membership_info[group]['documents']
+        assert len(documents) > nonmember_base_size
+        np.random.shuffle(documents)
+        final_documents = documents[:nonmember_base_size]
+        final_membership = [False] * nonmember_base_size
+
+        documents = membership_info[other_group]['documents']
+        assert len(documents) > member_pollution_size
+        np.random.shuffle(documents)
+        final_documents.extends(documents[:member_pollution_size])
+        final_membership.extends([True] * member_pollution_size)
+
+        membership_info[group]['documents'] = final_documents
+        membership_info[group]['is_members'] = final_documents
+        new_membership_info[group] = membership_info[group]
+        assert len(new_membership_info[group]['documents']) == size, (len(new_membership_info[group]['documents']), size)
+        assert len(new_membership_info[group]['is_members']) == size, (len(new_membership_info[group]['is_members']), size)
+
 
     # Create statistic info
     print("Calculating the statistics...")
@@ -183,8 +294,8 @@ def main(args):
         # "groups": group_rates
     }
     print(stats)
-    # with open(os.path.join(save_dir, 'grouping_stats.json'), "w") as f:
-    #     json.dump(stats, f, default=default, indent=4)
+    with open(os.path.join(new_save_dir, 'grouping_stats.json'), "w") as f:
+        json.dump(stats, f, default=default, indent=4)
     # draw_histogram(group_lengths_member + group_lengths_nonmember, title=None, xlabel="# documents each date", ylabel="# Dates(k)",
     #                 save_path=os.path.join(save_dir, 'documents_date_distribution.png'), bins=50)
     # draw_histogram(list(group_rates.values()), title=None, xlabel="Percentage of Members", ylabel="# Dates(k)",
@@ -193,8 +304,8 @@ def main(args):
     # if data_type.startswith("wikipedia"):
     #     draw_separate_histogram(scores_and_group, split=["1960", "2010", "2020-03-01", "2024"], xlabel="Percentage of duplication", ylabel="# Documents(k)",
     #                                 save_path=os.path.join(save_dir, 'group_bff_distribution.png'), bins=20)
-    # with open(membership_info_path, "wb") as f:
-    #     pkl.dump(membership_info, f)
+    with open(new_membership_info_path, "wb") as f:
+        pkl.dump(new_membership_info, f)
 
 
 if __name__ == '__main__':
@@ -203,7 +314,10 @@ if __name__ == '__main__':
     parser.add_argument('--data_type', type=str, default="rpj-arxiv")
     parser.add_argument('--document_threshold', type=float, default="0.5")
     parser.add_argument('--member_mix_ratio', type=float, default="0.5")
-    parser.add_argument('--nonmember-mix_ratio', type=float, default="0.5")
+    parser.add_argument('--nonmember_mix_ratio', type=float, default="0.5")
+    parser.add_argument('--member_group_size', type=int, default=50)
+    parser.add_argument('--nonmember_group_size', type=int, default=50)
+    parser.add_argument('--size', type=int, default="50")
 
     args = parser.parse_args()
 
