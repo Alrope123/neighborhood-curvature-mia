2b380e58579ef7b47b80f8545c2eb7708341de1d
Shane Lyu <alrope@uw.edu>
2024-02-16 10:59:47 -0800

    added debugging code

---
+++ debug/check_book_titles.py
@@ -0,0 +1,28 @@
+import json
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default="/gscratch/h2lab/alrope/data/redpajama/arxiv/")
+    parser.add_argument('--key', type=str, default=None)
+    args = parser.parse_args()
+
+    short_book_titles = set()
+    titles = set()
+    with open(args.data_path, 'r') as f:
+        # Load the JSONL file
+        if args.data_path.endswith(".jsonl"):    
+            for i, line in enumerate(f):
+                dp = json.loads(line)
+                if 'title' in dp['meta']:
+                    titles.add(dp['meta']['title'])
+                else:
+                    titles.add(dp['meta']['short_book_title'])
+                if i > 20:
+                    break
+        else:
+            data = json.load(f)
+
+    print(titles)
+
