71b8562a1a8bbc56cf306c1cfead9dd4e3bf8dbe
Shane Lyu <alrope@uw.edu>
2024-02-20 11:27:26 -0800

    changed running script

--- MIA/run_mia_unified.py
+++ MIA/run_mia_unified.py
@@ -1233,6 +1233,7 @@ if __name__ == '__main__':
     parser.add_argument('--save_dir', type=str, default="results")
 
     parser.add_argument('--min_k_prob', default=False, action='store_true')
+    parser.add_argument('--name', type=str, default=None)
 
     args = parser.parse_args()
 
@@ -1287,12 +1288,17 @@ if __name__ == '__main__':
     else:
         min_k_prob_string = ""
 
+    if args.name:
+        name_string = f'--{args.name}'
+    else:
+        name_string = ""
+
     dataset_member_name=args.dataset_member.replace('/', '_')
     dataset_nonmember_name=args.dataset_nonmember.replace('/', '_')
 
     # SAVE_FOLDER = f"tmp_results/{output_subfolder}{base_model_name}-{args.revision}{scoring_model_string}-{args.mask_filling_model_name}-{sampling_string}/{precision_string}-{args.pct_words_masked}-{args.n_perturbation_rounds}-{dataset_member_name}-{dataset_nonmember_name}-{args.n_group_member}-{args.n_group_nonmember}-{args.n_document_per_group}{ref_model_string}{span_length_string}{max_length_string}{tok_by_tok_string}"
     # SAVE_FOLDER = f"{args.save_dir}/{output_subfolder}{base_model_name}-{args.revision}{scoring_model_string}-{args.mask_filling_model_name}-{sampling_string}/{precision_string}-{args.pct_words_masked}-{args.n_perturbation_rounds}-{dataset_member_name}-{dataset_nonmember_name}-{args.n_group_member}-{args.n_group_nonmember}-{args.n_document_per_group}{ref_model_string}{span_length_string}{max_length_string}{tok_by_tok_string}"
-    SAVE_FOLDER = f"{args.save_dir}/{dataset_member_name}-{args.n_group_member}-{args.n_group_nonmember}-{args.n_document_per_group}{max_length_string}/{base_model_name}{min_k_prob_string}"
+    SAVE_FOLDER = f"{args.save_dir}/{dataset_member_name}-{args.n_group_member}-{args.n_group_nonmember}-{args.n_document_per_group}{max_length_string}{name_string}/{base_model_name}{min_k_prob_string}"
 
     # new_folder = SAVE_FOLDER.replace("tmp_results", args.save_dir)
     # ##don't run if exists!!!
