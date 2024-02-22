37dc8b1f65fec9bccec9a9f6031617708180cd85
Shane Lyu <alrope@uw.edu>
2024-02-22 03:37:36 -0800

    added debugging code

--- case_study/format_dataset.py
+++ case_study/format_dataset.py
@@ -45,8 +45,11 @@ if __name__ == '__main__':
         else:
             raise NotImplementedError('Dataset splits: {}'.format(huggingface_datasets))
         
+        print(type(huggingface_data))
+        assert False
+
         new_dataset = []
-        for i, entry in enumerate(huggingface_data.items()):
+        for i, entry in enumerate(huggingface_data):
             if not args.include_answer:
                 if "question" in entry:
                     if type(entry["question"]) == str:
@@ -87,6 +90,69 @@ if __name__ == '__main__':
 
             new_entry  = {"group": eval_dataset, "text": text}
             new_dataset.append(new_entry)
+
+        # if not args.include_answer:
+        #     if "question" in huggingface_data:
+        #         if type(entry["question"]) == str:
+        #             text = entry["question"]
+        #         elif type(entry["question"]) == dict:
+        #             if "text" in entry["question"]:
+        #                 text = entry["question"]["text"]
+        #             else:
+        #                 raise NotImplementedError('Entry fields: {}'.format(entry["question"]))
+        #     elif "text" in huggingface_data:
+        #         text = entry["text"]
+        #     elif "question_stem" in huggingface_data:
+        #         text = entry["question_stem"]
+        #     elif "question_stem" in huggingface_data:
+        #         text = entry["question_stem"]
+        #     elif "goal" in huggingface_data:
+        #         text = entry["goal"]
+        #     else:
+        #         raise NotImplementedError('Entry fields: {}'.format(entry))
+        #     dataset_key = 
+        # for i, entry in enumerate(huggingface_data):
+            
+        #         if "question" in entry:
+        #             if type(entry["question"]) == str:
+        #                 text = entry["question"]
+        #             elif type(entry["question"]) == dict:
+        #                 if "text" in entry["question"]:
+        #                     text = entry["question"]["text"]
+        #                 else:
+        #                     raise NotImplementedError('Entry fields: {}'.format(entry["question"]))
+        #         elif "text" in entry:
+        #             text = entry["text"]
+        #         elif "question_stem" in entry:
+        #             text = entry["question_stem"]
+        #         elif "question_stem" in entry:
+        #             text = entry["question_stem"]
+        #         elif "goal" in entry:
+        #             text = entry["goal"]
+        #         else:
+        #             raise NotImplementedError('Entry fields: {}'.format(entry))
+        #     else:
+        #         splitter = '\n'
+        #         if eval_dataset.startswith("allenai/ai2_arc"):
+        #             text = entry["question"] + splitter + entry["choices"]["text"][entry["answerKey"]]
+        #         elif eval_dataset.startswith("google/boolq"):
+        #             text = entry["question"] + splitter + entry["question"]
+        #         elif eval_dataset.startswith("gsm8k"):
+        #             text = entry["question"] + splitter + entry["answer"]
+        #         elif eval_dataset.startswith("lambada"):
+        #             text = entry["text"]
+        #         elif eval_dataset.startswith("natural_questions"):
+        #             text = entry["question"]["text"]
+        #         elif eval_dataset.startswith("openbookqa"):
+        #             text = entry["question_stem"] + " " + entry["choices"]["text"][entry["choices"]["label"].index(entry["answerKey"])]
+        #         elif eval_dataset.startswith("piqa"):
+        #             text = entry["goal"] + splitter + entry["sol{}".format(entry["label"])]
+        #         elif eval_dataset.startswith("cais/mmlu"):
+        #             text = entry["question"] + splitter + entry["choices"][choices_list.index(entry["answerKey"])]
+
+        #     new_entry  = {"group": eval_dataset, "text": text}
+        #     new_dataset.append(new_entry)
+
 
         with open(os.path.join("/gscratch/h2lab/alrope/data/eval{}/{}.jsonl".format("_full" if args.include_answer else "", eval_dataset)), 'w') as f:
             for entry in new_dataset:
