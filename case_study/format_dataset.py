eea12c5395693e9e262905067923581bee08e9a2
Shane Lyu <alrope@uw.edu>
2024-02-22 16:46:00 -0800

    fixed bug

--- case_study/format_dataset.py
+++ case_study/format_dataset.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 
     eval_datasets = ["allenai/ai2_arc@ARC-Easy@test", "google/boolq@validation", "gsm8k@main@test", "lambada@test", "natural_questions@default@validation", "openbookqa@test", "piqa@test"]
     mmlu_subsets = ['abstract_algebra', 'anatomy', 'astronomy', 'business_ethics', 'clinical_knowledge', 'college_biology', 'college_chemistry', 'college_computer_science', 'college_mathematics', 'college_medicine', 'college_physics', 'computer_security', 'conceptual_physics', 'econometrics', 'electrical_engineering', 'elementary_mathematics', 'formal_logic', 'global_facts', 'high_school_biology', 'high_school_chemistry', 'high_school_computer_science', 'high_school_european_history', 'high_school_geography', 'high_school_government_and_politics', 'high_school_macroeconomics', 'high_school_mathematics', 'high_school_microeconomics', 'high_school_physics', 'high_school_psychology', 'high_school_statistics', 'high_school_us_history', 'high_school_world_history', 'human_aging', 'human_sexuality', 'international_law', 'jurisprudence', 'logical_fallacies', 'machine_learning', 'management', 'marketing', 'medical_genetics', 'miscellaneous', 'moral_disputes', 'moral_scenarios', 'nutrition', 'philosophy', 'prehistory', 'professional_accounting', 'professional_law', 'professional_medicine', 'professional_psychology', 'public_relations', 'security_studies', 'sociology', 'us_foreign_policy', 'virology', 'world_religions']
-    eval_datasets.extend(["cais/mmlu@{}".format(subset) for subset in mmlu_subsets])
+    eval_datasets.extend(["cais/mmlu@{}@test".format(subset) for subset in mmlu_subsets])
 
     choices_list = ["A", "B", "C", "D"]
 
